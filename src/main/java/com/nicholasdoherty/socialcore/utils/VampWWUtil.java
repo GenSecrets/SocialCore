821776e61878ac8d7a1ce15e81fcb93c61e3e867
John Pascoe <pascoej@murri.ca>
2017-03-29 23:28:20 -0400

    Fix because of changes in werewolves

--- src/main/java/com/nicholasdoherty/socialcore/utils/VampWWUtil.java
+++ src/main/java/com/nicholasdoherty/socialcore/utils/VampWWUtil.java
@@ -1,9 +1,10 @@
 package com.nicholasdoherty.socialcore.utils;
 
 import com.massivecraft.vampire.entity.UPlayer;
-import com.nicholasdoherty.werewolf.core.WPlayer;
-import com.nicholasdoherty.werewolf.core.storage.WStore;
-import com.nicholasdoherty.werewolf.util.Lang;
+
+import com.nicholasdoherty.werewolf.Lang;
+import com.nicholasdoherty.werewolf.WPlayer;
+import com.nicholasdoherty.werewolf.WStore;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
