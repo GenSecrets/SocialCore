24ddca10e3bd7b553749f76924507f793bc74263
amy null <null@amy.gg>
2018-04-03 15:19:59 -0400

    /forceemote extensions

--- src/main/java/com/nicholasdoherty/socialcore/emotes/ForceEmoteCommand.java
+++ src/main/java/com/nicholasdoherty/socialcore/emotes/ForceEmoteCommand.java
@@ -2,6 +2,7 @@ package com.nicholasdoherty.socialcore.emotes;
 
 import com.nicholasdoherty.socialcore.NearbyAPI;
 import com.nicholasdoherty.socialcore.SocialCore;
+import com.nicholasdoherty.socialcore.emotes.extend.EmoteExtender;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -15,68 +16,72 @@ import org.bukkit.entity.Player;
 /**
  * Created by john on 5/2/15.
  */
+@SuppressWarnings("unused")
 public class ForceEmoteCommand implements CommandExecutor {
-    private SocialCore plugin;
-    private Emotes emotes;
-
-    public ForceEmoteCommand(SocialCore plugin) {
+    private final EmoteExtender extender = new EmoteExtender();
+    @SuppressWarnings("FieldCanBeLocal")
+    private final SocialCore plugin;
+    private final Emotes emotes;
+    
+    public ForceEmoteCommand(final SocialCore plugin) {
         this.plugin = plugin;
-        this.emotes = plugin.emotes;
+        emotes = plugin.emotes;
         plugin.getCommand("forceemote").setExecutor(this);
     }
-
+    
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        if (args.length < 2) {
+    public boolean onCommand(final CommandSender commandSender, final Command command, final String s, final String[] args) {
+        if(args.length < 2) {
             commandSender.sendMessage("Usage: /forceemote <player> <emote>");
             return true;
         }
-        Player player = Bukkit.getPlayer(args[0]);
-        if (player == null || !player.isOnline()) {
+        final Player player = Bukkit.getPlayer(args[0]);
+        if(player == null || !player.isOnline()) {
             commandSender.sendMessage(ChatColor.RED + "Could not find player: " + args[0]);
             return true;
         }
-        String emoteCommand = "/"+args[1].toLowerCase();
-        Emote emote = emotes.getEmote(emoteCommand);
-        if (emote == null) {
-            commandSender.sendMessage(ChatColor.RED +"Could not find emote: " + args[1]);
+        final String emoteCommand = '/' + args[1].toLowerCase();
+        final Emote emote = emotes.getEmote(emoteCommand);
+        if(emote == null) {
+            commandSender.sendMessage(ChatColor.RED + "Could not find emote: " + args[1]);
             return true;
         }
-        if (args.length > 2) {
-            Player target = Bukkit.getPlayer(args[2]);
-            if (!emote.isCanBetargeted()) {
+        if(args.length > 2) {
+            final Player target = Bukkit.getPlayer(args[2]);
+            if(!emote.isCanBetargeted()) {
                 commandSender.sendMessage(ChatColor.RED + "This emote may be not be targeted.");
                 return true;
             }
-            if (target == null || !target.isOnline() || VanishWrapper.isVanished(target)) {
+            if(target == null || !target.isOnline() || VanishWrapper.isVanished(target)) {
                 commandSender.sendMessage(ChatColor.RED + "Target " + args[2] + " is not currently online.");
                 return true;
             }
-            String message = emote.getEmoteMessageTargeted(player, target);
-            sendMessage(player,message);
-        }else {
-            if (!emote.isCanBeUntargeted()) {
+            final String message = emote.getEmoteMessageTargeted(player, target);
+            sendMessage(player, message);
+        } else {
+            if(!emote.isCanBeUntargeted()) {
                 commandSender.sendMessage(ChatColor.RED + "This emote must be targeted.");
                 return true;
             }
-            String message = emote.getEmoteMessage(player);
-            sendMessage(player,message);
+            final String message = emote.getEmoteMessage(player);
+            sendMessage(player, extender.process(message));
         }
-        commandSender.sendMessage(ChatColor.GREEN +"Did the emote");
+        commandSender.sendMessage(ChatColor.GREEN + "Did the emote");
         return true;
     }
-    public void sendMessage(Player p, String message) {
-        int radius = SocialCore.plugin.emotes.getRadius();
-        World world = p.getWorld();
-        if (radius == 0) {
-            for (Player toSend : world.getPlayers()) {
+    
+    @SuppressWarnings("TypeMayBeWeakened")
+    public void sendMessage(final Player p, final String message) {
+        final int radius = SocialCore.plugin.emotes.getRadius();
+        final World world = p.getWorld();
+        if(radius == 0) {
+            for(final Player toSend : world.getPlayers()) {
                 toSend.sendMessage(message);
             }
         } else {
-            for (Entity e : NearbyAPI.getNearbyPlayers(p.getLocation(),radius)) {
-                if (e.getType().equals(EntityType.PLAYER)) {
-                    Player toSend = (Player) e;
-                    toSend.sendMessage(message);
+            for(final Entity e : NearbyAPI.getNearbyPlayers(p.getLocation(), radius)) {
+                if(e.getType() == EntityType.PLAYER) {
+                    e.sendMessage(message);
                 }
             }
             //p.sendMessage(message);
