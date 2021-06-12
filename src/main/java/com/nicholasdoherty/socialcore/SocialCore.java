e444f483692254dbea3beb9be6dced6836e2fee8
Gen <secrets.of.gen@gmail.com>
2021-06-12 07:59:45 -0400

    Finalize removal of Races, Global Mute, Titles, Emotes, Gender from social core via updating the Main class

--- src/main/java/com/nicholasdoherty/socialcore/SocialCore.java
+++ src/main/java/com/nicholasdoherty/socialcore/SocialCore.java
@@ -2,27 +2,16 @@ package com.nicholasdoherty.socialcore;
 
 import com.nicholasdoherty.socialcore.courts.Courts;
 import com.nicholasdoherty.socialcore.courts.inputlib.InputLib;
-import com.nicholasdoherty.socialcore.emotes.EmoteCommand;
-import com.nicholasdoherty.socialcore.emotes.EmoteListener;
-import com.nicholasdoherty.socialcore.emotes.Emotes;
-import com.nicholasdoherty.socialcore.emotes.ForceEmoteCommand;
-import com.nicholasdoherty.socialcore.genders.GenderCommandHandler;
-import com.nicholasdoherty.socialcore.genders.Genders;
 import com.nicholasdoherty.socialcore.marriages.*;
-import com.nicholasdoherty.socialcore.misc.GlobalMute;
-import com.nicholasdoherty.socialcore.races.Race;
-import com.nicholasdoherty.socialcore.races.Races;
 import com.nicholasdoherty.socialcore.store.SQLStore;
 import com.nicholasdoherty.socialcore.time.Clock;
 import com.nicholasdoherty.socialcore.time.condition.TimeConditionManager;
-import com.nicholasdoherty.socialcore.titles.TitleManager;
 import com.voxmc.voxlib.gui.InventoryGUIManager;
 import com.voxmc.voxlib.util.VaultUtil;
 import com.voxmc.voxlib.util.VaultUtil.NotSetupException;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
@@ -45,26 +34,18 @@ public class SocialCore extends JavaPlugin {
     
     //save
     public SaveHandler save;
-    public Races races;
     //players
     public Map<String, SocialPlayer> socialPlayersCache;
-    
-    //genders
-    public Genders genders;
     
     //marriages
     public Marriages marriages;
     public List<String> whitelistPiggybackWorlds;
     // globalmute
-    public GlobalMute globalMute;
-    //emotes
-    public Emotes emotes;
     public SQLStore store;
     private InventoryGUIManager inventoryGUIManager;
     private InputLib inputLib;
     private Courts courts;
     private TimeConditionManager timeConditionManager;
-    private TitleManager titleManager;
     
     public InventoryGUIManager getInventoryGUIManager() {
         return inventoryGUIManager;
@@ -99,8 +80,6 @@ public class SocialCore extends JavaPlugin {
         getLogger().info("Config checked!");
         final String directory = getDataFolder().toString();
         getLogger().info("Creating handlers...");
-        races = new Races(this);
-        getLogger().info("[SC Handler] Created races handler");
         save = new SaveHandler(directory, this);
         getLogger().info("[SC Handler] Created save handler");
         marriages = new Marriages(this);
@@ -109,19 +88,13 @@ public class SocialCore extends JavaPlugin {
         getLogger().info("[SC Handler] Created MySQL handler");
         courts = new Courts(this);
         getLogger().info("[SC Handler] Created courts handler");
-        globalMute = new GlobalMute(this);
-        getLogger().info("[SC Handler] Created global mute handler");
         whitelistPiggybackWorlds = getConfig().getStringList("piggyback-world-whitelist"); // Putting this here so I don't have to read from config a lot
         getLogger().info("[SC Handler] Created piggyback whitelist config");
         getLogger().info("Finished creating handlers!");
         //commands
         final CommandExecutor scCommandHandler = new SCCommandHandler(this);
         final CommandExecutor marriageCommandHandler = new MarriageCommandHandler(this);
-        final CommandExecutor genderCommandHandler = new GenderCommandHandler(this);
         getCommand("socialcore").setExecutor(scCommandHandler);
-        getCommand("male").setExecutor(genderCommandHandler);
-        getCommand("female").setExecutor(genderCommandHandler);
-        getCommand("gender").setExecutor(genderCommandHandler);
         getCommand("marriage").setExecutor(marriageCommandHandler);
         getCommand("marriages").setExecutor(marriageCommandHandler);
         getCommand("engagements").setExecutor(marriageCommandHandler);
@@ -149,51 +122,11 @@ public class SocialCore extends JavaPlugin {
         //events
         getServer().getPluginManager().registerEvents(new SCListener(this), this);
         getLogger().info("Registered listeners!");
-        
-        //players
-//
-        //genders
-        genders = new Genders();
-        getLogger().info("Finished setting up genders!");
+
         
         //marriages
 //
-        //emotes
-        emotes = new Emotes(this);
-        new EmoteListener(this);
-        new EmoteCommand(this);
         new FixerCommand(this);
-        new ForceEmoteCommand(this);
-        getLogger().info("Finished setting up emotes!");
-        races.reloadRaces();
-        getLogger().info("Reloaded races!");
-        for(final Player p : Bukkit.getOnlinePlayers()) {
-            final SocialPlayer socialPlayer = save.getSocialPlayer(p.getName());
-            if(socialPlayer.getRace() != null) {
-                final Race race = socialPlayer.getRace();
-                PermissionAttachment permissionAttachment = null;
-                if(p.hasMetadata("pa")) {
-                    permissionAttachment = (PermissionAttachment) p.getMetadata("pa").get(0).value();
-                }
-                if(permissionAttachment == null) {
-                    permissionAttachment = p.addAttachment(this);
-                }
-//
-//
-                for(final String key : permissionAttachment.getPermissions().keySet()) {
-                    permissionAttachment.unsetPermission(key);
-                }
-                if(p.hasPermission("sc.race.issupernatural")) {
-                    socialPlayer.setRace(races.getDefaultRace().getName());
-                    races.getDefaultRace().applyRace(p, permissionAttachment);
-                    return;
-                }
-                race.applyRace(p, permissionAttachment);
-            }
-        }
-        getLogger().info("Updated online player races!");
-        titleManager = new TitleManager(this);
-        getLogger().info("Updated titles!");
         getLogger().info("SocialCore has finished starting!");
     }
     
@@ -212,8 +145,6 @@ public class SocialCore extends JavaPlugin {
             } catch(final Exception ignored) {
             }
         });
-        titleManager.onDisable();
-        titleManager.saveCacheFile();
     }
     
     @SuppressWarnings("unused")
@@ -277,9 +208,5 @@ public class SocialCore extends JavaPlugin {
             ex.printStackTrace();
             return false;
         }
-    }
-    
-    public enum Gender {
-        MALE, FEMALE, UNSPECIFIED
     }
 }
